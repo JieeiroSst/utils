3b8ba578bc57af360f1bdf1ee2aac2d0c3e854fb
Alain Liu <luumanhquan.91@gmail.com>
2025-03-19 09:10:39 +0700

    Update logger.go

--- logger/logger.go
+++ logger/logger.go
@@ -3,68 +3,130 @@ package logger
 import (
 	"context"
 	"os"
-	"time"
 
-	"github.com/google/uuid"
+	"github.com/JIeeiroSst/utils/trace_id"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-const (
-	Key = "TRANSACTION_ID_KEY"
-)
-
-func generateTransactionID() string {
-	return uuid.New().String()
+type Logger struct {
+	zap *zap.Logger
 }
 
-func TracerID(ctx context.Context) context.Context {
-	tid := generateTransactionID()
-	return context.WithValue(ctx, Key, tid)
+type Config struct {
+	Level      string
+	JSONFormat bool
+	AppName    string
 }
 
-func ToString(ctx context.Context) string {
-	tid := ctx.Value(Key)
-	return tid.(string)
+func New(config Config) (*Logger, error) {
+	level := zap.InfoLevel
+	if err := level.UnmarshalText([]byte(config.Level)); err != nil {
+		level = zap.InfoLevel
+	}
+
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "timestamp",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		FunctionKey:    zapcore.OmitKey,
+		MessageKey:     "message",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+
+	var encoder zapcore.Encoder
+	if config.JSONFormat {
+		encoder = zapcore.NewJSONEncoder(encoderConfig)
+	} else {
+		encoder = zapcore.NewConsoleEncoder(encoderConfig)
+	}
+
+	core := zapcore.NewCore(
+		encoder,
+		zapcore.AddSync(os.Stdout),
+		zap.NewAtomicLevelAt(level),
+	)
+
+	zapLogger := zap.New(
+		core,
+		zap.AddCaller(),
+		zap.AddCallerSkip(1),
+		zap.Fields(zap.String("app", config.AppName)),
+	)
+
+	return &Logger{zap: zapLogger}, nil
 }
 
-func ConfigZap(ctx context.Context) *zap.Logger {
-	ctx = TracerID(ctx)
-	config := zap.NewProductionConfig()
-	logger, _ := config.Build(zap.WrapCore(func(core zapcore.Core) zapcore.Core {
-		return zapcore.NewTee(core, zapcore.NewCore(
-			zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
-			zapcore.AddSync(zapcore.AddSync(zapcore.Lock(os.Stdout))),
-			zapcore.InfoLevel,
-		).With([]zapcore.Field{zap.String("transaction_id", ToString(ctx))}))
-	}))
-	return logger
+func (l *Logger) With(fields ...zapcore.Field) *Logger {
+	return &Logger{zap: l.zap.With(fields...)}
 }
 
-func SyslogTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
-	enc.AppendString(t.Format("2006-01-02 15:04:05"))
+func (l *Logger) WithContext(ctx context.Context) *Logger {
+	if ctx == nil {
+		return l
+	}
+
+	if tracerID := trace_id.GetTracerID(ctx); tracerID != "" {
+		return &Logger{zap: l.zap.With(zap.String("trace_id", tracerID.String()))}
+	}
+
+	return l
 }
 
-func CustomLevelEncoder(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
-	enc.AppendString("[" + level.CapitalString() + "]")
+func (l *Logger) Debug(msg string, fields ...zapcore.Field) {
+	l.zap.Debug(msg, fields...)
 }
 
-func Info(ctx context.Context, msg string, args ...interface{}) {
-	logger := ConfigZap(ctx)
-	logger.Sugar().Infof(msg, args...)
+func (l *Logger) Info(msg string, fields ...zapcore.Field) {
+	l.zap.Info(msg, fields...)
 }
 
-func Error(ctx context.Context, msg string, args ...interface{}) {
-	logger := ConfigZap(ctx)
-	logger.Sugar().Infof(msg, args...)
+func (l *Logger) Warn(msg string, fields ...zapcore.Field) {
+	l.zap.Warn(msg, fields...)
 }
 
-func Warn(ctx context.Context, msg string, args ...interface{}) {
-	logger := ConfigZap(ctx)
-	logger.Sugar().Infof(msg, args...)
+func (l *Logger) Error(msg string, fields ...zapcore.Field) {
+	l.zap.Error(msg, fields...)
 }
 
-func Debug(ctx context.Context, msg string, args ...interface{}) {
-	logger := ConfigZap(ctx)
-	logger.Sugar().Infof(msg, args...)
+func (l *Logger) Fatal(msg string, fields ...zapcore.Field) {
+	l.zap.Fatal(msg, fields...)
 }
+
+func (l *Logger) WithError(err error) *Logger {
+	if err == nil {
+		return l
+	}
+	return &Logger{zap: l.zap.With(zap.Error(err))}
+}
+
+func (l *Logger) Sync() error {
+	return l.zap.Sync()
+}
+
+var defaultLogger *Logger
+
+func InitDefault(config Config) {
+	logger, _ := New(config)
+
+	defaultLogger = logger
+}
+
+func Default() *Logger {
+	if defaultLogger == nil {
+		defaultLogger = &Logger{zap: zap.NewExample()}
+	}
+	return defaultLogger
+}
+
+func WithContext(ctx context.Context) *Logger {
+	return Default().WithContext(ctx)
+}
+
