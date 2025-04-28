794e1abaeb4d61be018f0b44ab0dc43cf12f3b75
Alain Liu <luumanhquan.91@gmail.com>
2025-04-28 21:36:07 +0700

    add code

--- cache/hash/hash.go
+++ cache/hash/hash.go
@@ -3,6 +3,7 @@ package hash
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"reflect"
 
 	"github.com/redis/go-redis/v9"
@@ -13,9 +14,11 @@ type cacheHelper struct {
 }
 
 type CacheHelper interface {
+	Get(ctx context.Context, key string, dest interface{}) error
 	GetInterface(ctx context.Context, key string, dataType interface{}) (map[string]interface{}, error)
+	Set(ctx context.Context, key string, value interface{}) error
 	SetInterface(ctx context.Context, key string, value map[string]interface{}) error
-	Removekey(ctx context.Context, key string) error
+	RemoveKey(ctx context.Context, key string) error
 }
 
 func NewCacheHelper(dns string) CacheHelper {
@@ -29,22 +32,53 @@ func NewCacheHelper(dns string) CacheHelper {
 	}
 }
 
+func (h *cacheHelper) Get(ctx context.Context, key string, dest interface{}) error {
+	data, err := h.redis.Get(ctx, key).Result()
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal([]byte(data), dest)
+}
+
 func (h *cacheHelper) GetInterface(ctx context.Context, key string, dataType interface{}) (map[string]interface{}, error) {
-	pipe := h.redis.Pipeline()
-	_, err := pipe.Exec(ctx)
+	fields := compileStructSpec(dataType)
+	if len(fields) == 0 {
+		return nil, fmt.Errorf("dataType must be a struct")
+	}
+
+	values, err := h.redis.HMGet(ctx, key, fields...).Result()
 	if err != nil {
 		return nil, err
 	}
 
-	fields := compileStructSpec(dataType)
-	cmd := pipe.HMGet(ctx, key, fields...)
+	result := make(map[string]interface{}, len(fields))
 
-	data := make(map[string]interface{}, 0)
-	if err := cmd.Scan(&data); err != nil {
-		return nil, err
+	for i, field := range fields {
+		if i < len(values) && values[i] != nil {
+			var v interface{}
+			strValue, isString := values[i].(string)
+			if isString {
+				if err := json.Unmarshal([]byte(strValue), &v); err != nil {
+					v = strValue
+				}
+			} else {
+				v = values[i]
+			}
+			result[field] = v
+		}
 	}
 
-	return data, nil
+	return result, nil
+}
+
+func (h *cacheHelper) Set(ctx context.Context, key string, value interface{}) error {
+	data, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+
+	return h.redis.Set(ctx, key, data, 0).Err()
 }
 
 func (h *cacheHelper) SetInterface(ctx context.Context, key string, value map[string]interface{}) error {
@@ -57,69 +91,91 @@ func (h *cacheHelper) SetInterface(ctx context.Context, key string, value map[st
 		}
 	}
 
-	err := h.redis.HMSet(ctx, key, marshaledValues).Err()
-	if err != nil {
-		return err
-	}
-	return nil
+	return h.redis.HSet(ctx, key, marshaledValues).Err()
 }
 
 func (h *cacheHelper) marshalValue(v interface{}) (interface{}, error) {
+	if v == nil {
+		return nil, nil
+	}
+
 	rv := reflect.ValueOf(v)
+	if rv.Kind() == reflect.Ptr {
+		if rv.IsNil() {
+			return nil, nil
+		}
+		return h.marshalValue(rv.Elem().Interface())
+	}
+
 	switch rv.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64, reflect.Bool:
+		return v, nil
+	case reflect.String:
+		return v, nil
 	case reflect.Struct:
-		dataJson, err := json.Marshal(&v)
+		data, err := json.Marshal(v)
 		if err != nil {
 			return nil, err
 		}
-		return dataJson, nil
-	case reflect.Slice:
-		array := make([]interface{}, 0, rv.Len())
-		for i := 0; i < rv.Len(); i++ {
-			elem, err := h.marshalValue(rv.Index(i).Interface())
-			if err != nil {
-				return nil, err
-			}
-			array = append(array, elem)
+		return string(data), nil
+	case reflect.Slice, reflect.Array:
+		data, err := json.Marshal(v)
+		if err != nil {
+			return nil, err
 		}
-		return array, nil
+		return string(data), nil
 	case reflect.Map:
-		mappedValues := make(map[string]interface{}, rv.Len())
-		for _, kk := range rv.MapKeys() {
-			elem, err := h.marshalValue(rv.MapIndex(kk).Interface())
-			if err != nil {
-				return nil, err
-			}
-			mappedValues[kk.Interface().(string)] = elem
+		data, err := json.Marshal(v)
+		if err != nil {
+			return nil, err
 		}
-		return mappedValues, nil
+		return string(data), nil
 	default:
-		return v, nil
+		data, err := json.Marshal(v)
+		if err != nil {
+			return nil, fmt.Errorf("unsupported type %s: %v", rv.Kind(), err)
+		}
+		return string(data), nil
 	}
 }
 
-func (h *cacheHelper) Removekey(ctx context.Context, key string) error {
-	if err := h.redis.Del(ctx, key).Err(); err != nil {
-		return err
-	}
-
-	return nil
+func (h *cacheHelper) RemoveKey(ctx context.Context, key string) error {
+	return h.redis.Del(ctx, key).Err()
 }
 
 func compileStructSpec(dataStr interface{}) []string {
-	array := make([]string, 0)
 	rv := reflect.ValueOf(dataStr)
+	
+	if rv.Kind() == reflect.Ptr {
+		if rv.IsNil() {
+			return nil
+		}
+		rv = rv.Elem()
+	}
+	
 	if rv.Kind() != reflect.Struct {
 		return nil
 	}
 
-	v := reflect.TypeOf(dataStr)
+	v := rv.Type()
+	fields := make([]string, 0, v.NumField())
 
 	for i := 0; i < v.NumField(); i++ {
 		field := v.Field(i)
+		
 		tag := field.Tag.Get("json")
-
-		array = append(array, tag)
+		if tag == "" || tag == "-" {
+			tag = field.Name
+		} else {
+			if comma := len(tag); comma >= 0 {
+				tag = tag[:comma]
+			}
+		}
+		
+		fields = append(fields, tag)
 	}
-	return array
+	
+	return fields
 }
